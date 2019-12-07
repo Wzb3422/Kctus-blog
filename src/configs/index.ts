14e19483a9f24cdf9c267674f1a9c37283aa3465
wzb <wzb3422@gmail.com>
2019-12-08 01:21:34 +0800

    :wrench: Config: add config

---
+++ src/configs/index.ts
@@ -0,0 +1,8 @@
+const info = {
+  name: 'Wzb',
+  introduction: 'Hi, I’m a theme for the Hexo blogging framework. I’m particularly great for a personal web page with a simple blog. In this paragraph, you can provide some information about yourself. Below, you’re most recent or highlighted writings and projects are listed.'
+}
+
+export {
+  info
+}
